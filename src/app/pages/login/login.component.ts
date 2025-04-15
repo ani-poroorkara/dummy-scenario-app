1c38a148b9ff00578c989631c880edd33ea841fd
Anirudh Poroorkara <anirudh_p@outlook.com>
2025-04-14 23:06:12 -0400

    login complete

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.username === 'admin' && this.password === 'admin') {
+      localStorage.setItem('loggedIn', 'true');
+      this.router.navigate(['/dashboard']);
+    } else {
+      alert('Invalid credentials!');
+    }
+  }
+}
