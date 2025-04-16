38b30e4a7500f8b577493abb2458e44b69f16b43
Anirudh Poroorkara <anirudh_p@outlook.com>
2025-04-16 16:53:12 -0400

    added some buttons

--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -1,24 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.scss'],
 })
 export class ResultsComponent {
+
+  scenarioName: string = 'Custom Scenario';
+  selectedComponents: string[] = [];
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      if (params['name'] !== undefined) {
+        this.scenarioName = params['name'];
+        this.selectedComponents = params['components'].split('>');
+      }
+    });
+  }
+
   scenarioSummary = {
-    name: 'My Custom Scenario',
-    components: ['C', 'S', 'B', 'R', 'T'],
     date: new Date().toLocaleString(),
   };
 
   reports = [
-    { name: 'Input Validation Report', file: 'input-validation.pdf' },
+    { name: 'Reconciliation Report', file: 'input-validation.pdf' },
     { name: 'Processing Summary', file: 'processing-summary.csv' },
-    { name: 'Final Output', file: 'output-report.xlsx' },
+    { name: 'Final Log', file: 'output-report.xlsx' },
   ];
 
   download(file: string) {
